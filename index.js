f005d7b6622c9c367332bcbec1ed32f3774b9648
Michelle <nyamburamichelle400@gmail.com>
2024-03-25 15:33:13 +0300

    completes assignments

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const  myArray = ["Guadalupe", "Ollie", "Aki"];
+let party = "surprise";
+
+function writeCards(myArray, party) {
+    let myArrayOne = [];
+    for ( let a = 0; a < myArray.length; a++) {
+
+        myArrayOne.push(`Thank you, ${myArray[a]}, for the wonderful ${party} gift!`);
+        //debugger;
+
+    }
+    return myArrayOne;
+}
+
+
+
+
+function countDown(myArray1) {
+    while(myArray1 >= 0){
+        console.log(myArray1)
+        myArray1--
+    }
+}
+countDown(4);
